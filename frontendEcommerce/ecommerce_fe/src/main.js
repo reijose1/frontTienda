64985cf0b0ba2ac97d1c09624dcc5d655d9c9e34
Sergio Medina <sergiomsystem@gmail.com>
2021-12-08 12:18:38 -0500

    Set up API GATEWAY connection with Vue

--- frontendEcommerce/ecommerce_fe/src/main.js
+++ frontendEcommerce/ecommerce_fe/src/main.js
@@ -7,4 +7,22 @@ import 'popper.js'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core';
+import { createApolloPrivider } from '@vue/apollo-option';
+import { setContext } from 'apollo-link-context';
+
+const httpLink = createHttpLink({
+    uri: 'https://doker-apis-p12c4g2.herokuapp.com/' // Url dónde está desplegada el API GATEWAY Apolo (Heroku)
+})
+
+const authLink = setContext((_, {headers}) => {
+    return {
+        headers: {
+            ...headers,
+            "Authorization": localStorage.getItem("token_access") || ""
+
+        }
+    }
+})
+
 createApp(App).use(store).use(router).mount('#app')
